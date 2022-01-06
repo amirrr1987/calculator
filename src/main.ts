dea5e9b160a86d249be0a4e6af74d5f979fe9f96
Amirrr1987 <Maghami.A1987@gmail.com>
2022-01-06 21:29:26 +0330

    feat: WindiCSS
    - install WindiCSS
    - import default config in vitejs
    - create WindiCSS config width config
    - import css in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "virtual:windi.css";
+import "virtual:windi-devtools";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount("#app");
