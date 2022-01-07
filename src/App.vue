81d2008253d2298544bb59259c666de6b84e549e
Amirrr1987 <Maghami.A1987@gmail.com>
2022-01-07 13:28:11 +0330

    feat: methods
    - set script area
    - use getNumber method to get btn value
    - use temp Array to store numbers
    - user monitorData computed to show store number

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,51 @@
 <template>
-  <div class="answer"></div>
-  <button class="btn btn__action btn__c">c</button>
+  <div class="answer">{{monitorData}}</div>
+  <button @click="cleaner" class="btn btn__action btn__c">c</button>
   <button class="btn btn__action btn__/">/</button>
   <button class="btn btn__action btn__=">=</button>
   <button class="btn btn__action btn__*">*</button>
   <button class="btn btn__action btn__-">-</button>
   <button class="btn btn__action btn__+">+</button>
   <button class="btn btn__action btn__.">.</button>
-  <button class="btn btn__number btn__9">9</button>
-  <button class="btn btn__number btn__8">8</button>
-  <button class="btn btn__number btn__7">7</button>
-  <button class="btn btn__number btn__6">6</button>
-  <button class="btn btn__number btn__5">5</button>
-  <button class="btn btn__number btn__4">4</button>
-  <button class="btn btn__number btn__3">3</button>
-  <button class="btn btn__number btn__2">2</button>
-  <button class="btn btn__number btn__1">1</button>
-  <button class="btn btn__number btn__0">0</button>
+  <button @click="getNumber(9)" class="btn btn__number btn__9">9</button>
+  <button @click="getNumber(8)" class="btn btn__number btn__8">8</button>
+  <button @click="getNumber(7)" class="btn btn__number btn__7">7</button>
+  <button @click="getNumber(6)" class="btn btn__number btn__6">6</button>
+  <button @click="getNumber(5)" class="btn btn__number btn__5">5</button>
+  <button @click="getNumber(4)" class="btn btn__number btn__4">4</button>
+  <button @click="getNumber(3)" class="btn btn__number btn__3">3</button>
+  <button @click="getNumber(2)" class="btn btn__number btn__2">2</button>
+  <button @click="getNumber(1)" class="btn btn__number btn__1">1</button>
+  <button @click="getNumber(0)" class="btn btn__number btn__0">0</button>
 </template>
+<script lang="ts">
+import { computed, defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "TheApp",
+  setup() {
+    const temp = ref<any>([])
+    const getNumber = (number: any) => {
+      temp.value.push(number);
+      console.log(number);
+      console.log(temp.value.join(''));
+      
+    };
+    const cleaner=()=>{
+      temp.value = []
+    }
+    const monitorData = computed(()=>{
+      return (+temp.value.join('')).toLocaleString()
+    })
+    return {
+      getNumber,
+      cleaner,
+      monitorData,
+
+    };
+  },
+});
+</script>
 <style>
 #app {
   @apply h-screen grid gap-4 p-4 text-white bg-[#60627b];
@@ -25,12 +53,15 @@
   grid-template-rows: repeat(8, 1fr);
 }
 .answer {
-  @apply bg-[#7b7c92] col-start-1 col-end-5 row-start-1 row-end-4;
+  @apply bg-[#7b7c92] col-start-1 col-end-5 row-start-1 row-end-4 text-5xl p-5;
   box-shadow: inset 0px 0px 4px #1e1f20, -2px -2px 10px #979393;
 }
 .btn {
-  @apply text-lg;
+  @apply text-2xl capitalize;
   box-shadow: 3px 3px 11px #1e1f20, -2px -2px 10px rgb(151, 147, 147);
+}
+.btn:active {
+  box-shadow: none;
 }
 .btn__number {
 }
