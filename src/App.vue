e18f4e590ddd9c669634ff42f3c034d616cdf068
Amir Maghami <maghami.a1987@gmail.com>
2022-01-23 16:31:59 +0330

    fix:  all methods
    - remove all methods
    - use esy methids

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,56 @@
 <template>
   <div class="answer">
-    <div >{{ monitorData }}</div>
-    <div >result : {{ resultData }}</div>
+    <span v-if="dataResult != ''">{{ dataResult }}</span>
+    <span v-else>0</span>
   </div>
   <button @click="cleaner" class="btn btn__action btn__c">c</button>
-  <button @click="calcOperator('/')" class="btn btn__action btn__/">/</button>
-  <button @click="calcOperator('*')" class="btn btn__action btn__*">*</button>
-  <button @click="calcOperator('-')" class="btn btn__action btn__-">-</button>
-  <button @click="calcOperator('+')" class="btn btn__action btn__+">+</button>
-  <button class="btn btn__action btn__.">.</button>
-  <button @click="calcData" class="btn btn__action btn__=">=</button>
-  <button @click="getNumber(9)" class="btn btn__number btn__9">9</button>
-  <button @click="getNumber(8)" class="btn btn__number btn__8">8</button>
-  <button @click="getNumber(7)" class="btn btn__number btn__7">7</button>
-  <button @click="getNumber(6)" class="btn btn__number btn__6">6</button>
-  <button @click="getNumber(5)" class="btn btn__number btn__5">5</button>
-  <button @click="getNumber(4)" class="btn btn__number btn__4">4</button>
-  <button @click="getNumber(3)" class="btn btn__number btn__3">3</button>
-  <button @click="getNumber(2)" class="btn btn__number btn__2">2</button>
-  <button @click="getNumber(1)" class="btn btn__number btn__1">1</button>
-  <button @click="getNumber(0)" class="btn btn__number btn__0">0</button>
+  <button @click="addToCalc('/')" class="btn btn--/">/</button>
+  <button @click="addToCalc('*')" class="btn btn--*">*</button>
+  <button @click="addToCalc('-')" class="btn btn---">-</button>
+  <button @click="addToCalc('+')" class="btn btn--+">+</button>
+  <button @click="addToCalc('.')" class="btn btn--.">.</button>
+  <button @click="calcData" class="btn btn--=">=</button>
+  <button @click="addToCalc('9')" class="btn btn--9">9</button>
+  <button @click="addToCalc('8')" class="btn btn--8">8</button>
+  <button @click="addToCalc('7')" class="btn btn--7">7</button>
+  <button @click="addToCalc('6')" class="btn btn--6">6</button>
+  <button @click="addToCalc('5')" class="btn btn--5">5</button>
+  <button @click="addToCalc('4')" class="btn btn--4">4</button>
+  <button @click="addToCalc('3')" class="btn btn--3">3</button>
+  <button @click="addToCalc('2')" class="btn btn--2">2</button>
+  <button @click="addToCalc('1')" class="btn btn--1">1</button>
+  <button @click="addToCalc('0')" class="btn btn--0">0</button>
 </template>
 <script lang="ts">
-import { computed, defineComponent, ref, watchEffect, } from "vue";
+import { computed, defineComponent, ref, watchEffect } from "vue";
 
 export default defineComponent({
   name: "TheApp",
   setup() {
-    let vh = computed(()=> window.innerHeight * 0.01);
-    
+    const vh = computed(() => window.innerHeight * 0.01);
 
-    watchEffect(()=> document.documentElement.style.setProperty('--vh', `${vh.value}px`))
+    watchEffect(() =>
+      document.documentElement.style.setProperty("--vh", `${vh.value}px`)
+    );
 
-
-    const temp = ref<any>([]);
-    const calcTemp = ref<any>([]);
-    const getNumber = (number: any) => {
-      temp.value.push(number);
-      console.log(temp.value.join(""));
+    const dataResult = ref("");
+    const addToCalc = (dataString: string) => {
+      dataString == "0" ? "" : (dataResult.value += dataString);
     };
-    const cleaner = () => {
-      temp.value = [];
-      calcTemp.value = [];
-      console.clear();
-       resultData.value = null
-    };
-    const calcOperator = (action: string) => {
-      let test = +temp.value.join("");
-      calcTemp.value.push(test);
-      calcTemp.value.push(action);
-      console.log(calcTemp.value);
-      temp.value = [];
-       resultData.value = null
-    };
-    // const monitorData = computed(() => {
-    //   return (+temp.value.join("")).toLocaleString();
-    // });
-
-    const monitorData = computed({
-      set: (value: any) => {
-        temp.value = +value;
-      },
-      get: () => {
-        return (+temp.value.join("")).toLocaleString();
-      },
-    });
-    const resultData = ref()
     const calcData = () => {
-      let test = +temp.value.join("");
-      calcTemp.value.push(test);
-      console.log(eval(calcTemp.value.join("")));
+      dataResult.value = eval(dataResult.value);
+    };
 
-      resultData.value = eval(calcTemp.value.join(""));
+    const cleaner = () => {
+      dataResult.value = "";
     };
 
     return {
-      getNumber,
       cleaner,
-      calcOperator,
-      monitorData,
       calcData,
-      resultData
+      addToCalc,
+      dataResult,
+      vh
     };
   },
 });
@@ -103,61 +73,56 @@ export default defineComponent({
 .btn:active {
   box-shadow: none;
 }
-.btn__number {
-}
-.btn__action {
-}
-.btn__c {
+
+.btn--c {
   @apply col-start-1 col-end-2 row-start-4 row-end-5;
 }
-.btn__\/ {
+.btn--\/ {
   @apply col-start-2 col-end-3 row-start-4 row-end-5;
 }
-.btn__\= {
+.btn--\= {
   @apply col-start-4 col-end-5 row-start-6 row-end-9;
 }
-.btn__\* {
+.btn--\* {
   @apply col-start-3 col-end-4 row-start-4 row-end-5;
 }
-.btn__\- {
+.btn--\- {
   @apply col-start-4 col-end-5 row-start-4 row-end-5;
 }
-.btn__\+ {
+.btn--\+ {
   @apply col-start-4 col-end-5 row-start-5 row-end-6;
 }
-.btn__\. {
+.btn--\. {
   @apply col-start-3 col-end-4 row-start-8  row-end-9;
 }
-.btn__0 {
+.btn--0 {
   @apply col-start-1 col-end-3 row-start-8 row-end-9;
 }
-.btn__1 {
+.btn--1 {
   @apply col-start-1 col-end-2 row-start-7 row-end-8;
 }
-.btn__2 {
+.btn--2 {
   @apply col-start-2 col-end-3 row-start-7 row-end-8;
 }
-.btn__3 {
+.btn--3 {
   @apply col-start-3 col-end-4 row-start-7 row-end-8;
 }
-
-.btn__4 {
+.btn--4 {
   @apply col-start-1 col-end-2 row-start-6 row-end-7;
 }
-.btn__5 {
+.btn--5 {
   @apply col-start-2 col-end-3 row-start-6 row-end-7;
 }
-.btn__6 {
+.btn--6 {
   @apply col-start-3 col-end-4 row-start-6 row-end-7;
 }
-
-.btn__7 {
+.btn--7 {
   @apply col-start-1 col-end-2 row-start-5 row-end-6;
 }
-.btn__8 {
+.btn--8 {
   @apply col-start-2 col-end-3 row-start-5 row-end-6;
 }
-.btn__9 {
+.btn--9 {
   @apply col-start-3 col-end-4 row-start-5 row-end-6;
 }
 </style>
