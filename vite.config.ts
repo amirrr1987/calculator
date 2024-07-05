9279d49ed9031abc7132db34605de4144735f378
Amir Maghami <Maghami.A1987@gmail.com>
2024-07-05 21:42:51 +0330

    :sparkles: add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 import UnoCSS from 'unocss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-     UnoCSS(),
-  ],
+  plugins: [vue(), UnoCSS(), VitePWA({ registerType: 'autoUpdate' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
